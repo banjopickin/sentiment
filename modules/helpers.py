ce54311f86a000333a74dee02d62b2726a88987c
banjopickin <yihualeng@gmail.com>
2016-02-21 21:28:46 -0800

    update

--- modules/helpers.py
+++ modules/helpers.py
@@ -1,4 +1,13 @@
 '''
 This documents contains helper functions
 '''
+import numpy as np
+
+def bag_stemer(stem_func,bag):
+    '''
+    stemming bag of words
+    :param stem_func: stemer function
+    :param bag: numpy arrary, bag of words
+    :return: np.array
+    '''
 
