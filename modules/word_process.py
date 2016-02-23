057dd49fcfaf4a4df0f2b5c30474162e6563e0aa
banjopickin <yihualeng@gmail.com>
2016-02-22 18:05:35 -0800

    processor in progress

---
+++ modules/word_process.py
@@ -0,0 +1,6 @@
+'''
+This document contains class function which can process raw string data followed by tokenize, stemming,
+lemmatizing and tf-idf vectorizing.
+'''
+
+class word_processor(object):
