9bdc0a946422e739d1cc213dcfea278bc66d55c7
banjopickin <yihualeng@gmail.com>
2016-02-24 17:50:59 -0800

    some update

--- modules/word_process.py
+++ modules/word_process.py
@@ -3,4 +3,43 @@ This document contains class function which can process raw string data followed
 lemmatizing and tf-idf vectorizing.
 '''
 
+from nltk import word_tokenize
+from helpers import *
+import numpy as np
+
 class word_processor(object):
+    '''
+    This class function process string array.
+    '''
+    def __init__(self, data, str_ind):
+        '''
+
+        :param data: numpy array
+        :param str_ind: int, indicate which column of data contains strings
+        :return: None
+        '''
+        self.data = data
+        self.words = data[:,str_ind]
+        self.fin_data = None
+
+    def tokenize(self):
+        '''
+        Tokeninze the words
+        :return: numpy array
+        '''
+        self.bag = np.array([word_tokenize(word) for word in self.words])
+
+    def stemming(self,stem_func):
+        '''
+        stemmer
+        :param stem_func: string. "snowball" or "porter"
+        :return: numpy array
+        '''
+        self.stemed_bag = bag_stemer(stem_func,self.bag)
+
+    def lemmatize(self):
+        '''
+        lemmatize strings
+        :return: numpy array
+        '''
+
